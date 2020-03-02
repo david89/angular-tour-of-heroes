9f9838de01b0816269b93a4d63e44d6862edcebe
David Gómez <gomez.david.89@gmail.com>
2020-03-01 20:27:53 -0800

    Add routing to the hero details component
    Now we can access hero details from the dashboard and the heroes list,
    instead of tying it to the heroes list view.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: "app-hero-detail",
@@ -8,9 +11,20 @@ import { Hero } from "../hero";
   styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.populateHero();
+  }
+
+  populateHero(): void {
+    const id: number = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,9 +11,15 @@ import { MessageService } from "./message.service";
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
+  getHero(id: number): Observable<Hero> {
+    var hero: Observable<Hero> = of(HEROES.find(hero => hero.id === id));
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return hero;
+  }
+
   getHeroes(): Observable<Hero[]> {
     var heroes: Observable<Hero[]> = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add("HeroService: fetched heroes");
     return heroes;
   }
 }
